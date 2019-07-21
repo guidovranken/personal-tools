e14aac543b7aa659ddcd0d882562900266969acd
Guido Vranken <guidovranken@gmail.com>
2019-07-21 23:01:35 +0200

    to_libfuzzer_dict.py: handle directories

--- to_libfuzzer_dict.py
+++ to_libfuzzer_dict.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
-import sys, struct
+import sys, struct, os, os.path
 
-with open(sys.argv[1], 'rb') as fp:
-    data = fp.read()
-    data = struct.pack('<I', len(data)) + data
-    s = "\""
-    for c in data:
-        s += "\\x"
-        s += hex(c)[2:].zfill(2)
-    s += "\""
-    print(s)
+def process_one(fn):
+    with open(fn, 'rb') as fp:
+        data = fp.read()
+        data = struct.pack('<I', len(data)) + data
+        s = "\""
+        for c in data:
+            s += "\\x"
+            s += hex(c)[2:].zfill(2)
+        s += "\""
+        print(s)
+
+infile = sys.argv[1]
+if os.path.isdir(infile):
+    files = [f for f in os.listdir(infile)]
+    for fn in files:
+        process_one(os.path.join(infile, fn))
+else:
+    process_one(infile)
